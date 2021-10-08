7ce6ebdf41b478401c4cf0e48c90e27770ffbdfa
manishcoder2212 <tengalemanish@gmail.com>
2021-10-08 07:46:47 -0700

    planet-app

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet,Header,TextInput,TouchableOpacity,Image,Alert,FlatList } from 'react-native';
+import axios from 'axios'
 
-export default function App() {
+export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      storage:[]
+    }
+  }
+    getImage=()=>{
+  axios
+  .get("http://localhost:5000/")
+  .then(response=>{
+              console.log(response.data.data[0].specifications)
+              this.setState({storage:response.data.data})
+
+  })
+  .catch(error=>{
+    Alert.alert(error.message)
+  })
+   }
+ componentDidMount(){
+ this.getImage();
+ 
+}
+
+renderItem = ({ item }) => (
+<View style={ item.specifications[1]=="goldilock"?styles.view: null}>
+  <Text>{item.specifications[1]=="goldilock"?item.name: null}</Text>
+   <Text>{item.specifications[1]=="goldilock"?item.planet_type: null}</Text>
+      <Text>{item.specifications[1]=="goldilock"?item.specifications: null}</Text>
+   </View>
+)
+
+  render(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Exo Planet App</Text>
+        <FlatList
+        data={this.state.storage}
+        renderItem={this.renderItem}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +55,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  view: {
+    marginTop:30,
+    backgroundColor:"#03fcf8"
+  }
 });
